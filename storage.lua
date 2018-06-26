14f97c1948e4b3c83f72bc2d670467faef84511e
entuland <entuland@gmail.com>
2018-06-26 12:54:48 +0200

    small clarificator comment

--- storage.lua
+++ storage.lua
@@ -1,4 +1,8 @@
 
+-- silly mistake: I used to create a folder named afte the module
+-- that conflicted with the file named in the same way created by get_mod_storage()
+-- fix: if present, rename such folder as wesh.temp_foldername
+
 local storage_folder = minetest.get_worldpath() .. "/mod_storage/"
 
 local dirs = minetest.get_dir_list(storage_folder, true)
